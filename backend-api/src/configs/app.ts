47c3b9066702e4ce39bf9b1ad6d1743bd6de9147
peterkitonga <pkitonga.pk@gmail.com>
2023-03-25 14:45:16 +0300

    KTR7 - Add config files for the app and logging

---
+++ backend-api/src/configs/app.ts
@@ -0,0 +1,12 @@
+export default {
+  env: process.env.NODE_ENV || 'development',
+  name: process.env.APP_NAME,
+  port: Number(process.env.APP_PORT),
+  base: process.env.APP_BASE_URL,
+  api: {
+    version: 'v1',
+    prefix(): string {
+      return `/api/${this.version}`;
+    },
+  },
+};
