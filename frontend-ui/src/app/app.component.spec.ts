96022d670b3b28a7ba06827f6590ef30c8896b8c
peterkitonga <pkitonga.pk@gmail.com>
2023-03-28 04:03:10 +0300

    KTR35 - Update spec file for the app component

--- frontend-ui/src/app/app.component.spec.ts
+++ frontend-ui/src/app/app.component.spec.ts
@@ -1,31 +1,25 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TranslateTestingModule } from 'ngx-translate-testing';
+
+import { AppComponent } from '@src/app/app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        TranslateTestingModule.withTranslations('en', require('../assets/i18n/en.json')),
+      ],
       declarations: [AppComponent],
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'frontend-ui'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend-ui');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend-ui app is running!');
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
   });
 });
