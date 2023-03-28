8ab0f1dbfaa14dfbb4e10bea15fed9c20022523d
peterkitonga <pkitonga.pk@gmail.com>
2023-03-28 06:26:56 +0300

    KTR39 - Add table component

--- frontend-ui/src/app/views/home/home.component.ts
+++ frontend-ui/src/app/views/home/home.component.ts
@@ -1,11 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+import { StockPrice } from '@src/app/shared/interfaces';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+  tableData!: StockPrice[];
+
+  cols!: { field: string; header: string }[];
+  menuItems!: { label: string; items: MenuItem[] }[];
+
+  first: number = 0;
+
+  rows: number = 10;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.tableData = [
+      {
+        id: 1,
+        companyName: 'Alphabet Inc.',
+        tickerSymbol: 'GOOG',
+        currentPrice: 222.22,
+        changePercent: 1.38,
+        createdAt: new Date().toLocaleString(),
+        updatedAt: new Date().toLocaleString(),
+      },
+    ];
+    this.cols = [
+      { field: 'companyName', header: 'Company Name' },
+      { field: 'tickerSymbol', header: 'Ticker Symbol' },
+      { field: 'currentPrice', header: 'Current Price' },
+      { field: 'changePercent', header: 'Change (in Percentage)' },
+      { field: '_', header: 'Actions' },
+    ];
+    this.menuItems = [
+      {
+        label: 'Actions',
+        items: [
+          {
+            label: 'Refresh Prices',
+            icon: 'bi bi-arrow-clockwise',
+            command: () => {
+              //
+            },
+          },
+          {
+            label: 'Add to List',
+            icon: 'bi bi-plus-lg',
+            command: () => {
+              //
+            },
+          },
+        ],
+      },
+    ];
+  }
+
+  onPageChange(event: { first: number; rows: number }) {
+    this.first = event.first;
+    this.rows = event.rows;
+  }
 }
