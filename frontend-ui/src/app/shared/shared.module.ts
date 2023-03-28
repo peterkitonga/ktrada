7c337c2fc82ec9291788a9c061c47f1a023f21db
peterkitonga <pkitonga.pk@gmail.com>
2023-03-28 06:26:15 +0300

    KTR39 - Import component modules to the shared module

--- frontend-ui/src/app/shared/shared.module.ts
+++ frontend-ui/src/app/shared/shared.module.ts
@@ -3,9 +3,38 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import { PaginatorModule } from 'primeng/paginator';
+import { ToastModule } from 'primeng/toast';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, CardModule],
-  exports: [CommonModule, ReactiveFormsModule, CardModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    CardModule,
+    TableModule,
+    PaginatorModule,
+    ToastModule,
+    ProgressBarModule,
+    AutoCompleteModule,
+    ButtonModule,
+    MenuModule,
+  ],
+  exports: [
+    CommonModule,
+    ReactiveFormsModule,
+    CardModule,
+    TableModule,
+    PaginatorModule,
+    ToastModule,
+    ProgressBarModule,
+    AutoCompleteModule,
+    ButtonModule,
+    MenuModule,
+  ],
 })
 export class SharedModule {}
